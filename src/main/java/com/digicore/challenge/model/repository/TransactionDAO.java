c52227db8e479c8499287427ecf542f5ac56695b
warl0ck1111 <bashirokala@gmail.com>
2021-06-08 18:22:44 +0100

    implemented transactionDAO functions

--- src/main/java/com/digicore/challenge/model/repository/TransactionDAO.java
+++ src/main/java/com/digicore/challenge/model/repository/TransactionDAO.java
@@ -6,20 +6,26 @@ import com.digicore.challenge.model.entity.Account;
 import com.digicore.challenge.model.entity.Transactions;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
 @Slf4j
 public class TransactionDAO {
 
-    public static HashMap<String, Transactions> transactions = new HashMap<>();
+    public static HashMap<Integer, Transactions> transactions = new HashMap<>();
 
-    boolean save(Transactions transactions){
-
+    public static boolean save(Transactions transaction){
+        transactions.put(transactions.size()+1,transaction);
         return true;
     }
 
-    List<Transactions> findAll(){
-        return null;
+    public static List<Transactions> findAll(){
+        List<Transactions> allTransactions = new ArrayList<>();
+        for(Integer key : transactions.keySet()){
+            Transactions transaction = TransactionDAO.transactions.get(key);
+            allTransactions.add(transaction);
+        }
+        return allTransactions;
     }
 }
